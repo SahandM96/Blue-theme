733e85790f54870f51f209615bf42bd38100b48b
sahandm96 <sahandm96@hotmail.com>
2016-10-18 18:39:17 +0330

    windows changes 6:39 1/18/2016

--- news.php
+++ news.php
@@ -9,13 +9,14 @@ Template Name: News
   $args = array('category' => 21); // include category 9
   $custom_posts = get_posts($args);
   foreach($custom_posts as $post) : setup_postdata($post);?>
-	<h2><?php the_title(); ?></h2>
+	<h2 style="text-shadow:none;"><?php the_title(); ?></h2>
   <div class="entry">
 
     <?php the_content(); ?>
 
     <?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
 
+    <hr>
 
   </div>
 
